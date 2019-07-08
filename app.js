87eff543973621c78daca74d0a373418a9b71493
Radeonxz <xuanxxzhao@gmail.com>
2019-07-07 22:18:16 -0400

    add mongoose populate

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const User = require('./models/user');
 
 const app = express();
 
+const user = userId => {
+  return User.findById(userId).then(user => {
+    return { ...user._doc, _id: user.id };
+  }).catch(err => {
+    console.log(err);
+    throw err;
+  });
+}
+
 // Parse incoming request
-
-
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
     type Event {
@@ -20,12 +27,14 @@ app.use('/graphql', graphqlHttp({
       description: String!
       price: Float!
       date: String!
+      creator: User!
     }
 
     type User {
       _id: ID!
       email: String!
       password: String
+      createdEvents: [Event!]
     }
 
     input EventInput {
@@ -58,9 +67,16 @@ app.use('/graphql', graphqlHttp({
   // resolvers
   rootValue: {
     events: () => {
-      return Event.find().then(events => {
+      return Event.find().populate('creator').then(events => {
         return events.map(event => {
-          return { ...event._doc, _id: event.id };
+          return {
+            ...event._doc,
+            _id: event.id,
+            creator: {
+              ...event._doc.creator._doc,
+              _id: event._doc.creator.id
+            }
+          };
         });
       }).catch(err => {
         console.log(err);
