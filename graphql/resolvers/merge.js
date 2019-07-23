efa980a497a407f82720b6a0770feb8dfb08b84c
Radeonxz <xuanxxzhao@gmail.com>
2019-07-22 21:14:55 -0400

    fix import error

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,3 +1,7 @@
+const Event = require('../../models/event');
+const User = require('../../models/user');
+const { dateToString } = require('../../helpers/date');
+
 const events = async eventsId => {
   try{
     const events = await Event.find({_id: {$in: eventsId}});
@@ -8,7 +12,7 @@ const events = async eventsId => {
     console.log(err);
     throw err;
   }
-}
+};
 
 const singleEvent = async eventId => {
   try{
@@ -18,7 +22,7 @@ const singleEvent = async eventId => {
     console.log(err);
     throw err;
   }
-}
+};
 
 const user = async userId => {
   try{  
@@ -32,8 +36,31 @@ const user = async userId => {
     console.log(err);
     throw err;
   }
-}
+};
 
-exports.user = user;
-exports.events = events;
-exports.singleEvent = singleEvent;
+const transformEvent = event => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: dateToString(event._doc.date),
+    creator: user.bind(this, event.creator)
+  };
+};
+
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
+  };
+};
+
+exports.transformEvent = transformEvent;
+exports.transformBooking = transformBooking;
+
+// exports.user = user;
+// exports.events = events;
+// exports.singleEvent = singleEvent;
