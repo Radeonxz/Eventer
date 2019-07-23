6d9a6ad92c3d60d443436a4a46f6639d07eb285c
Radeonxz <xuanxxzhao@gmail.com>
2019-07-22 20:51:25 -0400

    refactoring

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,3 +1,53 @@
 const Event = require('../../models/event');
 
 const { dateToString } = require('../../helpers/date');
+
+const transformEvent = event => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: dateToString(event._doc.date),
+    creator: user.bind(this, event.creator)
+  };
+};
+
+module.exports = {
+  events: async () => {
+    try{
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch(err) {
+      console.log(err);
+      throw err;
+    }
+  },
+  createEvent: async (args) => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5d241ecf3df8220cb495ee82'
+    });
+
+    let createdEvent;
+    try{
+    const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById('5d241ecf3df8220cb495ee82')
+
+      if(!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch(err) {
+      console.log(err);
+      throw err;
+    }
+  }
+}
