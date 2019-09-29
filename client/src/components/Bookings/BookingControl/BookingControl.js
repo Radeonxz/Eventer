6183eb5dca34e44a3934ee522b27f63643e469ba
Readonxz <xuanxxzhao@gmail.com>
2019-09-28 20:02:39 -0400

    booking control in progress

--- client/src/components/Bookings/BookingControl/BookingControl.js
+++ client/src/components/Bookings/BookingControl/BookingControl.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './BookingControl.css';
 
 const bookingControl = props => {
   <div className='booking-control'>
-    <button onClick={this.changeOutputTypeHandler.bind(this, 'list')}>List</button>
-    <button onClick={this.changeOutputTypeHandler.bind(this, 'chart')}>Chart</button>
+    <button
+      className={props.activeOutputType === 'list' ? 'active' : ''}
+      onClick={props.onChange.bind(this, 'list')}
+    >
+      List
+    </button>
+    <button
+      className={props.activeOutputType === 'chart' ? 'active' : ''}
+      onClick={props.onChange.bind(this, 'chart')}
+    >
+      Chart
+    </button>
   </div>
 }
 
