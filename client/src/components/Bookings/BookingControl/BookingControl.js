0b0e7b5791b9f76caf610bf77e19585730e8f417
Radeonxz <xuanxxzhao@gmail.com>
2020-03-12 18:28:46 -0400

    fix: booking render

--- client/src/components/Bookings/BookingControl/BookingControl.js
+++ client/src/components/Bookings/BookingControl/BookingControl.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './BookingControl.css';
+import React from "react";
+import "./BookingControl.css";
 
 const bookingControl = props => {
-  <div className='booking-control'>
-    <button
-      className={props.activeOutputType === 'list' ? 'active' : ''}
-      onClick={props.onChange.bind(this, 'list')}
-    >
-      List
-    </button>
-    <button
-      className={props.activeOutputType === 'chart' ? 'active' : ''}
-      onClick={props.onChange.bind(this, 'chart')}
-    >
-      Chart
-    </button>
-  </div>
-}
+  return (
+    <div className="booking-control">
+      <button
+        className={props.activeOutputType === "list" ? "active" : ""}
+        onClick={props.onChange.bind(this, "list")}
+      >
+        List
+      </button>
+      <button
+        className={props.activeOutputType === "chart" ? "active" : ""}
+        onClick={props.onChange.bind(this, "chart")}
+      >
+        Chart
+      </button>
+    </div>
+  );
+};
 
 export default bookingControl;
