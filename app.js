2981b5a274739047d1db453dab9cfed2e31faaac
Radeonxz <xuanxxzhao@gmail.com>
2020-05-02 14:55:33 -0400

    fix: mongodb deprecation warning

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const graphqlHttp = require('express-graphql');
-const mongoose = require('mongoose');
+const express = require("express");
+const graphqlHttp = require("express-graphql");
+const mongoose = require("mongoose");
 
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middlewares/is-auth');
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+const isAuth = require("./middlewares/is-auth");
 
 const app = express();
 
 // Parse incoming request
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Method', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if(req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
@@ -26,21 +26,27 @@ app.use((req, res, next) => {
 // User auth middleware for all routes
 app.use(isAuth);
 
-app.use('/graphql', graphqlHttp({
-  schema: graphQlSchema,
-  rootValue: graphQlResolvers,
-  graphiql: true
-}));
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true,
+  })
+);
 
+// MongoDB, server setup
 const MongoDBOptions = {
   useNewUrlParser: true,
-  useCreateIndex: true
-}
-
-mongoose.connect(process.env.MONGODB_URI, MongoDBOptions)
-.then(() => {
-  console.log('server is listening on 8000');
-  app.listen(8000);
-}).catch((err) => {
-  console.log(err);
-});
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(process.env.MONGODB_URI, MongoDBOptions)
+  .then(() => {
+    console.log("server is listening on 8000");
+    app.listen(8000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
