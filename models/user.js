6ac0eb6ac8e6dc03a7c9c7e47d2b699ae54db989
RadeonXz <xuanxxzhao@gmail.com>
2021-09-02 22:15:24 -0400

    chore: update model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {type: String, required: true},
-  password: {type: String, unique: true, required: true},
-  createdEvents: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'GraphEvent'
-    }
-  ]
+	email: { type: String, required: true },
+	password: { type: String, unique: true, required: true },
+	createdEvents: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Event"
+		}
+	]
 });
 
-module.exports = mongoose.model('GraphUser', userSchema);
+module.exports = mongoose.model("User", userSchema);
